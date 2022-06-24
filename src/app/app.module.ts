b49ed96cce5fc5507e759b15ff8a45c4677067c6
tomananze1997 <78642908+tomananze1997@users.noreply.github.com>
2022-06-24 15:46:06 +0200

    added routing and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { UserAlbumComponent } from './components/userComponents/user-album/user-album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    UsersComponent,
+    UserComponent,
+    AlbumsComponent,
+    PhotosComponent,
+    PostsComponent,
+    PostComponent,
+    UserAlbumComponent
   ],
   imports: [
     BrowserModule,
